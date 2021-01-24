7f32a0db0f62020eca5382cd569771a70baaf1e9
由幸 大坪 <eeeelllleeee24@gmail.com>
2021-01-24 19:35:46 +0900

    NULLとかの動きが‘Parseとコンバートで変わってくるよ

---
+++ CS10/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double d1 = 1.4d;
+            int aaa = (int)d1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string value = "10";
+            int intValue = int.Parse(value);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string value = "10";
+            int intValue;
+            bool result = int.TryParse(value, out intValue);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string value = "10";
+            int intValue = Convert.ToInt32(value);
+        }
+    }
+}

0259ae925c6931de4b4f9273fa8f02331b3d7d1a
由幸 大坪 <eeeelllleeee24@gmail.com>
2021-01-26 22:13:30 +0900

    動的配列はListですわ

---
+++ CS12/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<int> values = new List<int>();
+            values.Add(10);
+            values.Add(20);
+            values.Add(50);
+
+            MessageBox.Show(values.Count.ToString());
+            MessageBox.Show(values[2].ToString());
+
+            values.RemoveAt(3);
+
+            values.Remove(20);
+
+            values.Clear();
+        }
+    }
+}

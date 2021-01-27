a604b97045290ad64d338115f25b78b346c761e3
由幸 大坪 <eeeelllleeee24@gmail.com>
2021-01-27 23:29:28 +0900

    計算

---
+++ CS13/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i = 0;
+            i += 2;
+            //i = i + 2;
+
+            i -= 1;
+            // i = i - 1;
+
+            int a = 10 % 4;
+
+            if (a == 2)
+            {
+
+            }
+
+            if (a != 2)
+            {
+
+            }
+
+            if (a > 2)
+            {
+
+            }
+
+            if (a <= 2)
+            {
+
+            }
+
+            if (a == 2 && i == 3)
+            {
+
+            }
+
+            if (a == 2 || i == 3)
+            {
+
+            }
+
+            if ((a == 2 || i == 3)  && a > 1)
+            {
+
+
+            }
+        }
+}

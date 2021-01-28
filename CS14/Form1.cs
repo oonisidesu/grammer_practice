7c0795ade668382bb5ec41036fab41f2ea7f6929
由幸 大坪 <eeeelllleeee24@gmail.com>
2021-01-28 23:21:46 +0900

    条件

---
+++ CS14/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int value = Convert.ToInt32(textBox1.Text);
+            //if (value == 10)
+            //{
+            //    MessageBox.Show("10です");
+            //}
+            //else if (value == 20)
+            //{
+            //    MessageBox.Show("20です");
+            //}
+            //else if (value == 21)
+            //{
+            //    MessageBox.Show("21です");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("どれでもない");
+            //}
+
+            switch (value)
+            {
+                case 10:
+                    MessageBox.Show("10です");
+                    break;
+                case 11:
+                    MessageBox.Show("11です");
+                    break;
+                case 20:
+                    MessageBox.Show("20です");
+                    break;
+                case 21:
+                    MessageBox.Show("21です");
+                    break;
+                default:
+                    MessageBox.Show("どれでもない");
+                    break;
+            }
+        }
+    }
+}

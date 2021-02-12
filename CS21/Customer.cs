4b2e4e42336e647ff4377fd9cdcbbd79dc18a7f1
由幸 大坪 <eeeelllleeee24@gmail.com>
2021-02-12 21:02:11 +0900

    コンストラクタ

---
+++ CS21/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS21
+{
+    class Customer
+    {
+        public string FirstName;
+        public string LastName;
+
+        public Customer(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Customer(string lastName) : this("--", lastName)
+        {
+            LastName += "AAAAA";
+        }
+
+        public string GetFullName()
+        {
+            return LastName + FirstName;
+        }
+    }
+}
---
+++ CS21/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Customer customer = new Customer("大二郎");
+            //customer.FirstName = "大石";
+            //customer.LastName = "大二郎";
+            MessageBox.Show(customer.GetFullName());
+
+            Customer customer2 = new Customer("石", "二郎");
+            //customer2.FirstName = "石";
+            //customer2.LastName = "二郎";
+            MessageBox.Show(customer2.GetFullName());
+        }
+    }
+}

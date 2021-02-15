bf5a1d511f2dc1519ad0104837a8b78acd3de9ae
由幸 大坪 <eeeelllleeee24@gmail.com>
2021-02-15 21:58:27 +0900

    外から見られたくなければInternal

---
+++ CS22B/BPub.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS22B
+{
+    public class BPub
+    {
+
+        public void GetPublic()
+        {
+
+        }
+
+        internal void GetInternal()
+        {
+
+        }
+
+        protected void GetProtected()
+        {
+
+        }
+
+        internal protected void GetInternalProtected()
+        {
+
+        }
+
+        private void GetPrivate()
+        {
+
+        }
+    }
+}
---
+++ CS22B/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS22B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            BPub b = new BPub();
+            BInternal bInternal = new BInternal();
+            //外から見られたくなければInternal
+
+            b.GetPublic();
+            b.GetInternal();
+            b.GetInternalProtected();
+        }
+    }
+}
